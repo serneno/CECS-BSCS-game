0692e5b9fa1e213985f170c4f1e01484e97824f2
serneno <haydnphk@gmail.com>
2017-10-02 21:13:59 -0700

    Separate Game Control from main gain view file

--- ControlView.java
+++ ControlView.java
@@ -1,16 +1,48 @@
-//Code for displaying the game controls of the game
+import java.util.*;
 import java.awt.*;
 import javax.swing.*;
-public class ControlView {
-    ArrayList<String> rooms_available = new Arraylist<String>("Computer Lab", "Eat Club", "CECS Conference Room", "North Hall", "South Hall");
-    JButton draw_button = new JButton("Draw Card");
-    JButton move_button = new JButton("Move");
-    JButton play_button = new JButton("Play Card");
-    JList room_list = new JList(rooms_available.toArray());
-    room_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-    room_list.setLayoutOrientation(JList.VERTICAL);
-    room_list.setVisibleRowCount(8);
 
-    JScrollPane room_list_scroller = new JScrollPane(room_list);
-    //room_list_scroller.setPreferredSize(new Dimension(250, 80));
+public class ControlView {
+    //Returns the game control panel as JPanel
+    //Future iteration: parameter for an array with the rooms available
+    public JPanel display() {
+        JPanel control_view = new JPanel();
+        control_view.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        String[] rooms_available = {"Computer Lab", "Eat Club", "CECS Conference Room", "North Hall", "South Hall"};
+        JButton draw_button = new JButton("Draw Card");
+        JButton move_button = new JButton("Move");
+        JButton play_button = new JButton("Play Card");
+
+        //Rooms available scrollable list
+        JList<String> room_list = new JList<>(rooms_available);
+        room_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        room_list.setLayoutOrientation(JList.VERTICAL);
+        room_list.setVisibleRowCount(8);
+        JScrollPane room_list_scroller = new JScrollPane(room_list);
+        room_list_scroller.setPreferredSize(new Dimension(250, 80));
+
+        //Displays current statistics of the players
+        JTextArea current_stats = new JTextArea(
+            "\tLearning\tCraft\tIntegrity\tQuality Points\n" + 
+            "Matt\t6\t6\t6\t0\n" + 
+            "Amanda\t6\t6\t6\t0\n" +
+            "Habib\t6\t6\t6\t0\n\n" +
+            "Cards in deck:\t50" +
+            "\tDiscards out of play: \t0\n" +
+            "You are Amanda and you are in the South Hall" 
+        );
+        current_stats.setEditable(false);
+        JTextArea game_log = new JTextArea("Human player is Amanda"); 
+        game_log.setEditable(false);
+
+        //Adds all the components in the Game Control Panel
+        control_view.add(draw_button);
+        control_view.add(move_button);
+        control_view.add(play_button);
+        control_view.add(room_list_scroller);
+        control_view.add(current_stats);
+        control_view.add(game_log);
+        return control_view;
+    }
 }
--- GameView.java
+++ GameView.java
@@ -1,51 +1,8 @@
-import java.util.*;
 import java.awt.*;
 import javax.swing.*;
-
-public class GameView{
-    //Returns the game control panel as JPanel
-    //Future iteration: parameter for an array with the rooms available
-    public static JPanel ControlView() {
-        JPanel control_view = new JPanel();
-        control_view.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        String[] rooms_available = {"Computer Lab", "Eat Club", "CECS Conference Room", "North Hall", "South Hall"};
-        JButton draw_button = new JButton("Draw Card");
-        JButton move_button = new JButton("Move");
-        JButton play_button = new JButton("Play Card");
-
-        //Rooms available scrollable list
-        JList<String> room_list = new JList<>(rooms_available);
-        room_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        room_list.setLayoutOrientation(JList.VERTICAL);
-        room_list.setVisibleRowCount(8);
-        JScrollPane room_list_scroller = new JScrollPane(room_list);
-        room_list_scroller.setPreferredSize(new Dimension(250, 80));
-
-        //Displays current statistics of the players
-        JTextArea current_stats = new JTextArea(
-            "\tLearning\tCraft\tIntegrity\tQuality Points\n" + 
-            "Matt\t6\t6\t6\t0\n" + 
-            "Amanda\t6\t6\t6\t0\n" +
-            "Habib\t6\t6\t6\t0\n\n" +
-            "Cards in deck:\t50" +
-            "\tDiscards out of play: \t0\n" +
-            "You are Amanda and you are in the South Hall" 
-        );
-        current_stats.setEditable(false);
-        JTextArea game_log = new JTextArea("Human player is Amanda"); 
-        game_log.setEditable(false);
-
-        //Adds all the components in the Game Control Panel
-        control_view.add(draw_button);
-        control_view.add(move_button);
-        control_view.add(play_button);
-        control_view.add(room_list_scroller);
-        control_view.add(current_stats);
-        control_view.add(game_log);
-        return control_view;
-    }
-
+//Main class containg everything in the game view
+public class GameView {
+    // Adds all the components into the main window
     public static void addComponents(Container frame) {
         //Frame layout
         frame.setLayout(new BorderLayout());
@@ -54,25 +11,26 @@ public class GameView{
         JLabel map_img = new JLabel(csulb_map);
         JScrollPane map_scroll = new JScrollPane(map_img);
         map_scroll.setPreferredSize(new Dimension(map_scroll.getWidth(), 500));
-        //Game Controls Panel
-        JPanel game_control = ControlView();
+        // Game Controls Panel
+        ControlView gc = new ControlView();
+        JPanel game_control = gc.display();
         // Add JPanels into JFrame
         frame.add(map_scroll, BorderLayout.NORTH);
         frame.add(game_control, BorderLayout.SOUTH);
     }
  
-    //Creates the GUI for the game
+    // Creates the GUI for the game
     private static void displayGame() {
-        //Sets up the game frame
+        // Sets up the game frame
         JFrame frame = new JFrame("College Hustlers");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        //Sets the frame to fullsreen
+        // Sets the frame to fullsreen
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //Adds all the components into the main JFrame
+        // Adds all the components into the main JFrame
         addComponents(frame.getContentPane());
         // Arrange the components inside the window
         frame.pack();
-        // By default, the window is not visible. Make it visible.
+        // Makes the frame visible
         frame.setVisible(true);
     }
 
