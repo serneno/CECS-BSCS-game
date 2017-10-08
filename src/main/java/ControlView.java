50890693c4b14e2a18925011c8879bdcad41a8e1
serneno <haydnphk@gmail.com>
2017-10-08 00:28:21 -0700

    stuff

--- src/main/java/ControlView.java
+++ src/main/java/ControlView.java
@@ -11,14 +11,15 @@ public class ControlView {
         JPanel control_view = new JPanel();
         control_view.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        //c.anchor = GridBagConstraints.NORTHWEST;
+        //c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.NORTHWEST;
 
         String[] rooms_available = {"Computer Lab", "Eat Club", "CECS Conference Room", "North Hall", "South Hall"};
         
         JButton draw_button = new JButton("Draw Card");
         c.gridx = c.gridy = 0;
         c.insets = new Insets(0, 0, 10, 0);
+        c.weightx = 0;
         control_view.add(draw_button, c);
         
         JButton move_button = new JButton("Move");
@@ -42,10 +43,22 @@ public class ControlView {
 
         //Display current card
         JPanel current_card = new JPanel();
+        JTextArea card_placeholder = new JTextArea("This will contain a card.");
+        current_card.setBorder(BorderFactory.createLineBorder(Color.black));
+        current_card.add(card_placeholder);
+        c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 0;
+        c.gridheight = 3;
         control_view.add(current_card, c);
+
+        JPanel placeholder = new JPanel();
+        c.gridx = 1;
+        c.gridy = 4;
+        c.gridheight = 1;
+        control_view.add(placeholder, c);
         //Displays current statistics of the players
+        
         JTextArea current_stats = new JTextArea(
             "\tLearning\tCraft\tIntegrity\tQuality Points\n" + 
             "Matt\t6\t6\t6\t0\n" + 
@@ -56,13 +69,20 @@ public class ControlView {
             "You are Amanda and you are in the South Hall" 
         );
         current_stats.setEditable(false);
-        c.gridx = 3;
+        current_stats.setBorder(BorderFactory.createLineBorder(Color.black));
+        c.gridx = 2;
         c.gridy = 0;
+        c.gridheight = 2;
+        c.ipadx = c.ipady = 20;
+        c.insets = new Insets(10, 10, 10, 10);
+        c.weightx = 0.4;
         control_view.add(current_stats, c);
 
-        JTextArea game_log = new JTextArea("Human player is Amanda"); 
+        JTextArea game_log = new JTextArea("Human player is Amanda");
+        game_log.setBorder(BorderFactory.createLineBorder(Color.black)); 
         game_log.setEditable(false);
-        c.gridy = 1;
+        c.gridheight = 1;
+        c.gridy = 2;
         control_view.add(game_log, c);
         return control_view;
     }
